40cd5314e0a06951c951ac076075a28d7e8baad5
K.A.I.Dilum <isiradilumuni@gmail.com>
2025-12-01 14:31:15 +0530

    profilepage Ui owner/viewer

---
+++ .next/dev/static/chunks/src_app_page_tsx_027fdfa5._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_54132f22._.js",
+  "static/chunks/node_modules_a13886b7._.js"
+],
+    source: "dynamic"
+});
